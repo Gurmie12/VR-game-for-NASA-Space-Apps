8ba096905a2adf973c28b53fd330363528ee8bbe
Gurman Brar <g9brar@edu.uwaterloo.ca>
2020-01-13 01:31:09 -0500

    Final project

---
+++ Assets/CanvaseScript2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class CanvaseScript2 : MonoBehaviour
+{
+	public Text canvasText1;
+	void Start()
+	{
+		Invoke("DisableText", 100f);//invoke after 5 seconds
+	}
+	void DisableText()
+	{
+		canvasText1.enabled = false;
+	}
+}
